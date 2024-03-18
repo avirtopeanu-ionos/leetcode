fa7e62daff0feba93420b187f40c8f3d27d8073a
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-03-18 11:28:20 +0200

    add reverse in place for linked list

--- Linked List/reverse-linked-list.py
+++ Linked List/reverse-linked-list.py
@@ -25,7 +25,15 @@ class Solution:
 
         return ans
 
-
+    def reverseListInPlace(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        left_pointer = None
+        current = head
+        while current: # End when current pointer is None (left pointer is the last element)
+            right_pointer = current.next # advance our right pointer
+            current.next = left_pointer  # set the relationship 
+            left_pointer = current # advance our left pointer
+            current = right_pointer # advance our current pointer 
+        return left_pointer
 
 if __name__ == "__main__":
     def build_linked_list(values):
